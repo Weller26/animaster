52b0db2558910ecac1cf57c9858ac9841e3a1686
graphento <idc>
2026-03-10 17:21:58 +0500

    initial setup

--- index.js
+++ index.js
@@ -1,65 +1,53 @@
+function animaster() {
+    this._steps = [];
+
+	const prototype = {
+		move: function move(element, duration, translation) {
+			element.style.transitionDuration = `${duration}ms`;
+			element.style.transform = getTransform(translation, null);
+			return this;
+		},
+		fadeIn: function fadeIn(element, duration) {
+			element.style.transitionDuration = `${duration}ms`;
+			element.classList.remove("hide");
+			element.classList.add("show");
+			return this;
+		},
+		scale: function (element, duration, ratio) {
+			element.style.transitionDuration = `${duration}ms`;
+			element.style.transform = getTransform(null, ratio);
+		},
+	};
+
+	function getTransform(translation, ratio) {
+		const result = [];
+		if (translation) {
+			result.push(`translate(${translation.x}px,${translation.y}px)`);
+		}
+		if (ratio) {
+			result.push(`scale(${ratio})`);
+		}
+		return result.join(" ");
+	}
+
+	Object.setPrototypeOf(animaster.prototype, prototype);
+}
+
 addListeners();
 
 function addListeners() {
-    document.getElementById('fadeInPlay')
-        .addEventListener('click', function () {
-            const block = document.getElementById('fadeInBlock');
-            fadeIn(block, 5000);
-        });
+	document.getElementById("fadeInPlay").addEventListener("click", function () {
+		const block = document.getElementById("fadeInBlock");
+		animaster().fadeIn(block, 500);
+	});
 
-    document.getElementById('movePlay')
-        .addEventListener('click', function () {
-            const block = document.getElementById('moveBlock');
-            move(block, 1000, {x: 100, y: 10});
-        });
+	document.getElementById("movePlay").addEventListener("click", function () {
+		const block = document.getElementById("moveBlock");
+		animaster().move(block, 500, { x: 100, y: 10 });
+	});
 
-    document.getElementById('scalePlay')
-        .addEventListener('click', function () {
-            const block = document.getElementById('scaleBlock');
-            scale(block, 1000, 1.25);
-        });
-}
-
-/**
- * Блок плавно появляется из прозрачного.
- * @param element — HTMLElement, который надо анимировать
- * @param duration — Продолжительность анимации в миллисекундах
- */
-function fadeIn(element, duration) {
-    element.style.transitionDuration =  `${duration}ms`;
-    element.classList.remove('hide');
-    element.classList.add('show');
-}
-
-/**
- * Функция, передвигающая элемент
- * @param element — HTMLElement, который надо анимировать
- * @param duration — Продолжительность анимации в миллисекундах
- * @param translation — объект с полями x и y, обозначающими смещение блока
- */
-function move(element, duration, translation) {
-    element.style.transitionDuration = `${duration}ms`;
-    element.style.transform = getTransform(translation, null);
-}
-
-/**
- * Функция, увеличивающая/уменьшающая элемент
- * @param element — HTMLElement, который надо анимировать
- * @param duration — Продолжительность анимации в миллисекундах
- * @param ratio — во сколько раз увеличить/уменьшить. Чтобы уменьшить, нужно передать значение меньше 1
- */
-function scale(element, duration, ratio) {
-    element.style.transitionDuration =  `${duration}ms`;
-    element.style.transform = getTransform(null, ratio);
-}
-
-function getTransform(translation, ratio) {
-    const result = [];
-    if (translation) {
-        result.push(`translate(${translation.x}px,${translation.y}px)`);
-    }
-    if (ratio) {
-        result.push(`scale(${ratio})`);
-    }
-    return result.join(' ');
+	document.getElementById("scalePlay").addEventListener("click", function () {
+		const block = document.getElementById("scaleBlock");
+		animaster().scale(block, 500, 1.25);
+	});
 }
